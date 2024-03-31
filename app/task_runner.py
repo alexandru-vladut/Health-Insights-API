4002574e404075504a714988651fbb61f08422b0
Alexandru-Nicolae Vladut <alexnv@yahoo.com>
2024-03-31 10:23:37 +0300

    Implemented ThreadPool and TaskRunner classes

--- app/task_runner.py
+++ app/task_runner.py
@@ -1,6 +1,7 @@
 from queue import Queue
 from threading import Thread, Event
 import time
+import os
 
 class ThreadPool:
     def __init__(self):
@@ -12,17 +13,43 @@ class ThreadPool:
         # You must NOT:
         #   * create more threads than the hardware concurrency allows
         #   * recreate threads for each task
-        pass
+
+        # Define the number of threads to be used by the thread pool
+        num_threads = int(os.getenv('TP_NUM_OF_THREADS', os.cpu_count()))
+
+        # Define the job queue
+        self.job_queue = Queue()
+
+        # Create the workers
+        self.workers = [TaskRunner(self.job_queue) for _ in range(num_threads)]
+        
+        # Start the workers
+        for worker in self.workers:
+            worker.start()
+
+    # Add a job to the queue (called by the webserver when a new reqquest is made)
+    def add_job(self, job, job_id):
+        # A job consists of a closure and an id
+        self.job_queue.put((job, job_id))
+
 
 class TaskRunner(Thread):
-    def __init__(self):
+    def __init__(self, job_queue):
         # TODO: init necessary data structures
-        pass
+        super().__init__()
+        self.job_queue = job_queue
 
     def run(self):
         while True:
             # TODO
+
             # Get pending job
+            job, job_id = self.job_queue.get()
+
             # Execute the job and save the result to disk
+            job()
+
+            # Mark the job as done (decrement the queue's internal counter)
+            self.job_queue.task_done()
+
             # Repeat until graceful_shutdown
-            pass
