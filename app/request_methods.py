05ad1353481d464d6adacaf13378df510710b006
Alexandru-Nicolae Vladut <alexnv@yahoo.com>
2024-03-31 12:28:09 +0300

    Implemented state_diff_from_mean_request()

--- app/request_methods.py
+++ app/request_methods.py
@@ -6,6 +6,21 @@ import json
 import re
 import pandas
 
+def states_mean(question):
+    # Filter the DataFrame based on the question
+    df_filtered = webserver.data_ingestor.dataframe[
+        webserver.data_ingestor.dataframe['Question'] == question]
+
+    # Calculate the average of "Data_Value" for each state
+    states_mean = df_filtered.groupby('LocationDesc')['Data_Value'].mean().sort_values()
+
+    # Convert DataFrame to dictionary
+    result = states_mean.to_dict()
+
+    # Return result dictionary
+    return result
+
+
 def state_mean(question, state):
     # Filter the DataFrame based on the question and state
     df_filtered = webserver.data_ingestor.dataframe[
@@ -18,21 +33,6 @@ def state_mean(question, state):
 
     # Create result dictionary
     result = {state: average_value}
-
-    # Return result dictionary
-    return result
-
-
-def states_mean(question):
-    # Filter the DataFrame based on the question
-    df_filtered = webserver.data_ingestor.dataframe[
-        webserver.data_ingestor.dataframe['Question'] == question]
-
-    # Calculate the average of "Data_Value" for each state
-    states_mean = df_filtered.groupby('LocationDesc')['Data_Value'].mean().sort_values()
-
-    # Convert DataFrame to dictionary
-    result = states_mean.to_dict()
 
     # Return result dictionary
     return result
@@ -71,6 +71,23 @@ def diff_from_mean(question):
         # If the state mean value is not NaN, calculate the difference and add to dictionary
         if not pandas.isna(state_mean_value):
             result[state] = global_mean_value - state_mean_value
+
+    # Return result dictionary
+    return result
+
+
+def state_diff_from_mean(question, state):
+    # Get the global mean value
+    global_mean_value = global_mean(question)["global_mean"]
+
+    # Get the state mean value
+    state_mean_value = state_mean(question, state)[state]
+
+    # Create result dictionary
+    result = {}
+
+    # Calculate the difference and add to dictionary
+    result[state] = global_mean_value - state_mean_value
 
     # Return result dictionary
     return result
